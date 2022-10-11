802c110d6099303b01415456f2f9fa936ec2c394
Shoaib Ahmed <shoaibahmedgtk5@gmail.com>
2022-10-10 22:58:54 -0700

    hello: Working Fine

---
+++ app/src/main/java/com/example/fragment/Fragment.kt
@@ -0,0 +1,38 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Fragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_, container, false)
+    }
+
+
+}
